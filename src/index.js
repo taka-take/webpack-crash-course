87e7d4cda7fc8f53b5dbc1c58aade1d8e1470075
takatake <taka12hiro01.o.m@gmail.com>
2020-03-20 02:54:00 +0900

    implement sass-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import _ from 'lodash';
-import style from './style.css';
+import './style.css';
+import './style.scss';
 import logo from './logo.png';
-
-console.log(style.toString());
 
 const component = () => {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.(jpe?g|png|gif|svg|icon)$/i,
         loader: 'url-loader',
         options: {
