02bf9be555a9abdfd571b4d1f69ea35b478878af
takatake <taka12hiro01.o.m@gmail.com>
2020-03-21 00:43:12 +0900

    feat:apply mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const outputPath = path.resolve(__dirname, 'dist');
-console.log({ outputPath });
 
 module.exports = {
   entry: './src/index.js',
@@ -14,12 +13,8 @@ module.exports = {
     rules: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.(sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|png|gif|svg|icon)$/i,
@@ -42,6 +37,9 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
     }),
   ],
 };
